ac6cb3f24fbe090404a88008c5f18310c84d96a7
Nicolas Culotta <nicolas.culotta@avature.net>
2020-03-18 09:59:59 -0300

    dev

--- v2/scripts.js
+++ v2/scripts.js
@@ -10,7 +10,6 @@ $(document).ready(function () {
     let modal = $('.remodal').remodal()
 
    
-
     $('#buttonCreate').click( function(){
 
         let caseInfo = {
@@ -26,7 +25,6 @@ $(document).ready(function () {
         if (isValid) {
             modal.close()
             caseContainer.append(printCase(caseInfo))
-            recoverStatusCase()
         }
     })
 
@@ -60,71 +58,53 @@ $(document).ready(function () {
 
     function printCase(caseInfo){
 
-        /* let caseStatusSelected = caseStatusInput */
 
-        let caseTemplate = `<div class="case__container"><div class="case">
-                                <div class="case__item--client">
-                                    <input type="text" name="client" id="clientInput" value="${caseInfo.client}">
-                                </div>
-                                <div class="case__item--type">
-                                    <input type="text" name="type" id="typeInput" value="${caseInfo.type}">
-                                </div>
-                                <div class="case__item--date">${caseInfo.date}</div>
-                                <div class="case__item--status">
-                                    <select id="caseStatus">
-                                        <option value="A">Queue for Dev</option>
-                                        <option value="B">Sent to Case Owner</option>
-                                        <option value="C">Development</option>
-                                        <option value="D">Code Review</option>
-                                        <option value="E">First Review</option>
-                                        <option value="F">In QA</option>
-                                        <option value="G">Changes and Feedback</option>
-                                        <option value="H">Bugfixing</option>
-                                        <option value="I">Merged</option>
-                                    </select>
-                                </div>
-                                <div class="case__buttons">
-                                    <div class="case__buttons--icon--note">
-                                        <img src="../assets/file-text-outline.svg" alt="note">
+        let caseTemplate = `<div class="case__container">
+                                <div class="case">
+                                    <div class="case__item--client">
+                                        <input type="text" name="client" id="clientInput" value="${caseInfo.client}">
                                     </div>
-                                    <div class="case__buttons--icon--link">
-                                        <a href="https://teg.avature.net/Case#/${caseInfo.id}">
-                                            <img src="../assets/link-2-outline.svg" alt="link">
-                                        </a>
+                                    <div class="case__item--type">
+                                        <input type="text" name="type" id="typeInput" value="${caseInfo.type}">
                                     </div>
-                                    <div class="case__buttons--icon--remove">
-                                        <img src="../assets/trash-2-outline.svg" alt="remove">
+                                    <div class="case__item--date">${caseInfo.date}</div>
+                                    <div class="case__item--status">
+                                        <select id="caseStatus">
+                                            <option value="A">Queue for Dev</option>
+                                            <option value="B">Sent to Case Owner</option>
+                                            <option value="C">Development</option>
+                                            <option value="D">Code Review</option>
+                                            <option value="E">First Review</option>
+                                            <option value="F">In QA</option>
+                                            <option value="G">Changes and Feedback</option>
+                                            <option value="H">Bugfixing</option>
+                                            <option value="I">Merged</option>
+                                        </select>
                                     </div>
-                                </div>     
-                            </div>
-                            <div class="case__note">
-                                <input type="text" name="type" id="noteInput" value="${caseInfo.note}"></p>
-                            </div></div>`;
-        
+                                    <div class="case__buttons">
+                                        <div class="case__buttons--icon--note">
+                                            <img src="../assets/file-text-outline.svg" alt="note">
+                                        </div>
+                                        <div class="case__buttons--icon--link">
+                                            <a href="https://teg.avature.net/Case#/${caseInfo.id}">
+                                                <img src="../assets/link-2-outline.svg" alt="link">
+                                            </a>
+                                        </div>
+                                        <div class="case__buttons--icon--remove">
+                                            <img src="../assets/trash-2-outline.svg" alt="remove">
+                                        </div>
+                                    </div>     
+                                </div>
+                                <div class="case__note">
+                                    <input type="text" name="type" id="noteInput" value="${caseInfo.note}"></p>
+                                </div>
+                            </div>`;
+
         return caseTemplate
     }
 
-    function recoverStatusCase(){
-        
-        let caseStatusCard = $('#caseStatus')
-        statusValue = caseStatusInput.val()
+    function printNote(caseInfo){
 
-        if (statusValue === "A"){
-            caseStatusCard.attr("selected","selected");
-            console.log('seleccionaste a A' + caseStatusCard)
-        } else if (statusValue === "B"){
-            caseStatusCard.attr("selected","selected"); 
-            console.log('seleccionaste a B' + caseStatusCard)
-        }
-        else if (statusValue === "C"){
-            caseStatusCard.attr("selected","selected"); 
-            console.log('seleccionaste a A'  + caseStatusCard)
-        }
-        else if (statusValue === "D"){
-            caseStatusCard.attr("selected","selected"); 
-        }
-        
-        
+        let noteTemplate = 
     }
-
 });
