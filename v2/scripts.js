654c934e32b1d79bdd21e4084b7880d6922df77b
nicoculotta <nicoculotta@gmail.com>
2020-03-24 14:20:53 -0300

    no anda esta verga

--- v2/scripts.js
+++ v2/scripts.js
@@ -1,3 +1,4 @@
+let casesArray = []
 $(document).ready(function () {
 
     let clientNameInput = $('#clientNameInput')
@@ -9,6 +10,8 @@ $(document).ready(function () {
 
     
     let modal = $('.remodal').remodal()
+
+    let dataCounter = 0
 
    
     $('#buttonCreate').click( function(){
@@ -25,20 +28,21 @@ $(document).ready(function () {
         let isValid = validateForm()
         if (isValid) {
             modal.close()
+            casesArray.push(caseInfo)
             caseContainer.append(printCase(caseInfo))
+            dataCounter++ 
         }
     })
 
 
-    $('body').on('click', function(e) {
-        let caseNoteContainer = $('.case__note')
-        for (let i = 0; i < caseNoteContainer.length; i++){
-            if ($(e.target).hasClass('case__buttons--icon--note') || $(e.target)[0].tagName === 'IMG' ){
-
-                caseNoteContainer[i].toggle('slow', 'swing')
-            }
+     $('body').on('click', function(e) {
+        
+        if ($(e.target).hasClass('case__buttons--icon--note') || $(e.target)[0].tagName === 'IMG' ){
+            
+            $(this).find(".case__note").toggle()
         }
     })
+
 
 
     function validateForm(){
@@ -68,9 +72,25 @@ $(document).ready(function () {
         e.addClass('error');
     }
 
+/*     function hasSameId(id){
+        for (let i=0; i < casesArray.length; i++){
+            if (casesArray[i].id === id ){
+                addErrorClass(idCaseInput)
+                return true
+            }
+        }
+    } */
+
     function printCase(caseInfo){
 
-        let caseTemplate = `<div class="case__container">
+        let iconNoteTemplate = `<div class="case__buttons--icon--note has-note" data-counter="${dataCounter}">
+                                    <img src="../assets/file-text-outline.svg" alt="note" data-counter="${dataCounter}">
+                                </div>`;
+        let noteTemplate = `<div class="case__note" style="display:none;" data-counter="${dataCounter}">
+                                <input type="text" name="type" id="noteInput" value="${caseInfo.note}"></input>
+                            </div>`
+
+        let caseTemplate = `<div class="case__container" data-counter="${dataCounter}">
                                 <div class="case">
                                     <div class="case__item--client">
                                         <input type="text" name="client" id="clientInput" value="${caseInfo.client}">
@@ -93,9 +113,7 @@ $(document).ready(function () {
                                         </select>
                                     </div>
                                     <div class="case__buttons">
-                                        <div class="case__buttons--icon--note has-note">
-                                            <img src="../assets/file-text-outline.svg" alt="note">
-                                        </div>
+                                        ${iconNoteTemplate}
                                         <div class="case__buttons--icon--link">
                                             <a href="https://teg.avature.net/Case#/${caseInfo.id}">
                                                 <img src="../assets/link-2-outline.svg" alt="link">
@@ -106,9 +124,7 @@ $(document).ready(function () {
                                         </div>
                                     </div>     
                                 </div>
-                                <div class="case__note">
-                                    <input type="text" name="type" id="noteInput" value="${caseInfo.note}"></p>
-                                </div>
+                                ${noteTemplate}
                             </div>`;
         
         return caseTemplate
