7da93e0feb1d2649870c3ab49a3525732b72d53a
nicoculotta <nicoculotta@gmail.com>
2020-06-25 09:35:18 -0300

    vue

--- vueProject/app.js
+++ vueProject/app.js
@@ -6,7 +6,6 @@ new Vue({
             name: 'Bitcoin',
             img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Bitcoin.svg/1200px-Bitcoin.svg.png',
             valorBtc: -20,
-            precios: [1200,430,1293343,1235,59645],
             pricesWithDays: [
                 { day:'Lunes', value:8400 },
                 { day:'Martes', value:7900 },
@@ -15,7 +14,15 @@ new Vue({
                 { day:'Viernes', value:9400 },
                 { day:'Sabado', value:10000 },
                 { day:'Domingo', value:10200 },
-            ]
+            ],
+            showPrices: false,
+            elPrecio: 9000
+        }
+    },
+
+    methods: {
+        toggleShowPrices(){
+            this.showPrices = !this.showPrices
         }
     }
 })
