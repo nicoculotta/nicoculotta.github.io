476cba59de9938699c55755d287b5ac4f7211148
nicoculotta <nicoculotta@gmail.com>
2020-09-21 22:09:29 -0300

    twity with server

---
+++ TwityShare/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key:         'iCwLkzaq7hoNU9Z8UCYmFsljN',
+    consumer_secret:      'aklj4ws4yFUhERwywKew4n2iOZPaZ039MutiFLO7UXLWJcpp7H',
+    access_token:         '372359805-fZ2XgLWfVBtOsXvkU4iMr4hfVJGaVmtgmQActKsh',
+    access_token_secret:  'SzHAfq9LQBMdekGgR3cl91KNuCrBlLeuX1JwAEHtEhqrm'
+}
---
+++ TwityShare/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const Twit = require('twit');
+
+// API KEYS TWITTER saved in config.js
+const config = require('./config');
+const T = new Twit(config);
+
+
+const app = express();
+app.listen(3000, () => { 
+    console.log('el server esta OK')
+});
+app.use(express.static('public'));
+app.use(express.json());
+
+app.post('/api', (request, response) => {
+    // receive the id of the tweet
+    const tweetId = request.body.id
+    
+    // receive info of particular tweet
+    const receiveTweet = (err, data, response) => {
+        if (!err){
+            let tweet = data;
+            let tweetInformation = {
+                text: tweet.text,
+                name: tweet.user.name,
+                screen_name: "@" + tweet.user.screen_name,
+                profile_image_url: tweet.user.profile_image_url
+            }
+            console.log(tweetInformation);
+        }
+    }
+    
+    T.get(`statuses/show/${tweetId}`, receiveTweet);
+
+})
+
