b889dcda1fdc803f0d3b407a92a4c4465d6ab593
nicoculotta <nicoculotta@gmail.com>
2020-10-27 19:42:22 -0300

    changes

--- TwityShare/index.js
+++ TwityShare/index.js
@@ -13,25 +13,26 @@ app.listen(3000, () => {
 app.use(express.static('public'));
 app.use(express.json());
 
+var tweetInformation = null;
+
 app.post('/api', (request, response) => {
     // receive the id of the tweet
     const tweetId = request.body.id
-    
-    // receive info of particular tweet
-    const receiveTweet = (err, data, response) => {
-        if (!err){
-            let tweet = data;
-            let tweetInformation = {
-                text: tweet.text,
-                name: tweet.user.name,
-                screen_name: "@" + tweet.user.screen_name,
-                profile_image_url: tweet.user.profile_image_url
-            }
-            console.log(tweetInformation);
-        }
-    }
-    
+        
     T.get(`statuses/show/${tweetId}`, receiveTweet);
-
+    response.json(tweetInformation);
 })
 
+// receive info of particular tweet
+const receiveTweet = (err, data, response) => {
+    if (!err){
+        let tweet = data;
+        tweetInformation = {
+            text: tweet.text,
+            name: tweet.user.name,
+            screen_name: "@" + tweet.user.screen_name,
+            profile_image_url: tweet.user.profile_image_url
+        }
+        console.log(tweetInformation);
+    }
+}
